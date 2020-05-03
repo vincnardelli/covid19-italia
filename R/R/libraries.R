e276829d191c25eea916c36eeabd14e7dabede72
Vincenzo Nardelli <enardelli94@gmail.com>
2020-05-03 12:44:32 +0200

    added libraries

---
+++ R/R/libraries.R
@@ -0,0 +1,13 @@
+#libraries
+library(dplyr)
+library(writexl)
+library(deSolve)
+library(lubridate)
+library(tidyr)
+library(geojsonio)
+library(ggplot2)
+library(readr)
+library(openxlsx)
+library(stringr)
+
+install.packages(c("dplyr", "writexl", "deSolve", "lubridate", "tidyr", "geojsonio", "ggplot2", "readr", "openxlsx", "stringr"))
